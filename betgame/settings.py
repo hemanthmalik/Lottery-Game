db3771c0f8f5afbcc7a2e00e474a843976ca6433
Hemant Malik <hemant.malik@etmedialabs.com>
2021-03-27 16:41:21 +0530

    added staticfiles config

--- betgame/settings.py
+++ betgame/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import yaml
+import os
 
 yaml_file = open('creds.yaml', 'r')
 creds_yaml = yaml.load(yaml_file, Loader=yaml.SafeLoader)
@@ -133,3 +134,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
